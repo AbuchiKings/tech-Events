4d081ee697e87bf40a35fe59d7e89d6050ad86a1
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-02-25 22:05:01 +0100

    ch: Initialised initial state

---
+++ src/store/initState.js
@@ -0,0 +1,17 @@
+const state = {
+    eventsList: {
+        events: [],
+        isloading: false,
+        message: ''
+    },
+
+    user:{
+        isloggedIn: false,
+        firstName: undefined,
+        lastName: undefined,
+        username: undefined,
+        email: undefined,
+        id: undefined,
+        token: undefined
+    }
+}
